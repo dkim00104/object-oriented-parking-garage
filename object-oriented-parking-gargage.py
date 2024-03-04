0fbdefd467fe79fd80236512b80ed1bba703f49a
Tan Nguyen <tannguyen5659@gmail.com>
2024-03-04 10:21:43 -0800

    main() function to run with choices

--- object-oriented-parking-gargage.py
+++ object-oriented-parking-gargage.py
@@ -4,13 +4,15 @@ class Parking_garage():
         self.parkingSpaces = parkingSpaces
         self.currentTicket = {}
 
-    def takeTicket(self, license_plate):
+    def takeTicket(self):
         self.tickets -= 1
         self.parkingSpaces -= 1
+        license_plate = input('Enter your license plate: ')
         self.currentTicket[license_plate] = False
         print(f'Car licensed: {license_plate} just took a ticket')
 
-    def payForParking(self, license_plate):
+    def payForParking(self):
+        license_plate = input('Enter your license plate: ')
         print(f'{license_plate} has not paid for the ticket yet. The total amount is $10')
         while True:
             amount = int(input('Insert card or cash to pay for the ticket: Amount: '))
@@ -19,7 +21,8 @@ class Parking_garage():
                 self.currentTicket[license_plate] = True
                 break
 
-    def leaveGarage(self, license_plate):
+    def leaveGarage(self):
+        license_plate = input('Enter your license plate: ')
         if self.currentTicket[license_plate] == True:
             print('Thank you, have a nice day')
             self.tickets +=1
@@ -42,16 +45,47 @@ class Parking_garage():
     def check_spaces(self):
         print(f'Currently, the garage has {self.parkingSpaces} open spaces ')
 
-garage1 = Parking_garage(10,10)
-garage1.takeTicket('9zpo')
-garage1.takeTicket('912')
-garage1.check_spaces()
-garage1.leaveGarage('9zpo')
-garage1.takeTicket('9890')
-garage1.payForParking('912')
-garage1.leaveGarage('912')
-garage1.show_history()
-garage1.check_spaces()
+
+
+def main():
+    garage1 = Parking_garage(10,10)
+    while True:
+        print("\nParking Garage Menu:")
+        print("1. Take Ticket")
+        print("2. Pay for Parking")
+        print("3. Leave Garage")
+        print("4, Check Spaces")
+        print("5, Show history of the garage on the day")
+        print("6. Quit")
+        choice = input("Enter your choice (1-6): ")
+        if choice == '1':
+            garage1.takeTicket()
+        elif choice == '2':
+            garage1.payForParking()
+        elif choice == '3':
+            garage1.leaveGarage()
+        elif choice == '4':
+            garage1.check_spaces()
+        elif choice == '5':
+            garage1.show_history()
+        elif choice == '6':
+            print("Exiting program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 4.")
+
+main()
+
+# garage1 = Parking_garage(10,10)
+# garage1.takeTicket('9zpo')
+# garage1.takeTicket('912')
+# garage1.check_spaces()
+# garage1.leaveGarage('9zpo')
+# garage1.takeTicket('9890')
+# garage1.payForParking('912')
+# garage1.leaveGarage('912')
+# garage1.show_history()
+# garage1.check_spaces()
 
 
             
